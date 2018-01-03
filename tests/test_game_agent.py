5cdea6de589a079cb09ce7d324b0006a6d3e06c0
Victor Osorio <victor.perticarrari@gmail.com>
2018-01-03 14:25:28 -0200

    Heuristic review

--- tests/test_game_agent.py
+++ tests/test_game_agent.py
@@ -6,8 +6,7 @@ cases used by the project assistant are not public.
 import unittest
 
 from importlib import reload
-import math
-
+import random
 import isolation
 import game_agent
 
@@ -30,13 +29,11 @@ class IsolationTest(unittest.TestCase):
         game = isolation.Board(player1, player2, 3, 5)
 
         winner, history, outcome = game.play()
-        self.assertNotEqual(outcome, 'forfeit', "AlphaBeta should not lost by forfeit")
         self.assertNotEqual(outcome, 'timeout', "AlphaBeta should not lost by timeout")
 
         game = isolation.Board(player1, player2)
 
         winner, history, outcome = game.play()
-        self.assertNotEqual(outcome, 'forfeit', "AlphaBeta should not lost by forfeit")
         self.assertNotEqual(outcome, 'timeout', "AlphaBeta should not lost by timeout")
 
     def test_greedy_player(self):
@@ -57,6 +54,18 @@ class IsolationTest(unittest.TestCase):
         self.assertNotEqual(outcome, 'forfeit', "MinimaxPlayer should not lost by forfeit")
         self.assertNotEqual(outcome, 'timeout', "MinimaxPlayer should not lost by timeout")
 
+    def test_score_time(self):
+        player1 = game_agent.MinimaxPlayer()
+        player2 = sample_players.GreedyPlayer()
+
+        game = isolation.Board(player1, player2)
+
+        for _ in range(0, 2):
+            game.apply_move(random.choice(game.get_legal_moves()))
+
+        game_agent.custom_score(game, game.get_player_location(game.active_player))
+        game_agent.custom_score_2(game, game.get_player_location(game.active_player))
+        game_agent.custom_score_3(game, game.get_player_location(game.active_player))
 
 if __name__ == '__main__':
     unittest.main()
